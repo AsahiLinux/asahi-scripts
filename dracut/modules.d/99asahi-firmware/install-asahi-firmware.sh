b800dd7685e3ebb83ad62c30c93240a8e54844e6
Hector Martin <marcan@marcan.st>
2025-01-28 20:57:42 +0900

    dracut: Use `cp -a` to preserve hard links
    Saves ~45MB of RAM.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- dracut/modules.d/99asahi-firmware/install-asahi-firmware.sh
+++ dracut/modules.d/99asahi-firmware/install-asahi-firmware.sh
@@ -9,4 +9,4 @@ if [ ! -d /sysroot/lib/firmware/vendor ]; then
     return 1
 fi
 mount -t tmpfs -o mode=0755 vendorfw /sysroot/lib/firmware/vendor
-cp -pr /vendorfw/* /vendorfw/.vendorfw.manifest /sysroot/lib/firmware/vendor
+cp -a /vendorfw/* /vendorfw/.vendorfw.manifest /sysroot/lib/firmware/vendor
